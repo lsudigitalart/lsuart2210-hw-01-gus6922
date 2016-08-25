22af9a4c4a7eaac1072e6bfb619c4f585c4ac438
Gus Lange <guslange@6c-40-8-a3-a7-b6.wlan.lsu.edu>
2016-08-25 11:24:02 -0500

    DONE

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(1000, 1000);
+
+    ellipse(100, 100, 50);
+    fill(0,0,0);
+    ellipse(100,100,25);
+
+    fill(255,255,255);
+    ellipse(200, 100, 50);
+    fill(0,0,0);
+    ellipse(200,100,25);
+
+    fill(255,255,255);
+    ellipse(150,150,100,50);
+    fill(0,0,0);
+    ellipse(150,161,50,25);
+
+    fill(255,255,255);
+    ellipse(150,154,5,10);
+}
